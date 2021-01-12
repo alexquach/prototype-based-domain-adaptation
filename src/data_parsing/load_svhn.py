70fd564af1240f839b7c6a79e6c0f9c533234a18
Alex Quach <alexhq1000@gmail.com>
2021-01-12 17:57:07 -0500

    Add Street View House Number (SVHN) dataset loading

---
+++ src/data_parsing/load_svhn.py
@@ -0,0 +1,24 @@
+from torch.utils.data import DataLoader
+from torchvision.datasets import SVHN
+from torchvision.transforms import Grayscale, Compose, CenterCrop, ToTensor
+
+def load_svhn_dataloader(bs=64, greyscale=False):
+    if greyscale:
+        transform = Compose([
+            CenterCrop(28),
+            Grayscale,
+            ToTensor()
+        ])
+    else:
+        transform = Compose([
+            CenterCrop(28),
+            ToTensor()
+        ])
+
+    train_ds = SVHN("data/svhn", split='train', transform=transform, download=True)
+    test_ds = SVHN("data/svhn", split='test', transform=transform, download=True)
+
+    train_dl = DataLoader(train_ds, batch_size=bs, shuffle=True)
+    test_dl = DataLoader(test_ds, batch_size=bs * 2)
+
+    return train_dl, test_dl
