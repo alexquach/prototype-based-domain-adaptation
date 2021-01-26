ab55f8197a5de9ecd979961559eca67b7b6e6a1b
Alex Quach <alexhq1000@gmail.com>
2021-01-26 03:38:42 -0500

    Initial CycleModel with only unsupervised cycle-loss consistency when training samples

---
+++ src/models/cycle_model.py
@@ -0,0 +1,206 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+import numpy as np
+
+from models.proto_model import ProtoModel
+from utils.plotting import plot_rows_of_images
+
+class CycleModel(nn.Module):
+    def __init__(self, source_model, target_model, epochs=10, weights=(1,1)):
+        super().__init__()
+        self.dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.source_model = source_model
+        self.target_model = target_model
+        self.epoch = 0
+        self.epochs = epochs
+
+        # Transfer layer
+        # Currently using linear
+        self.transition_model = nn.Linear(source_model.latent_dim, target_model.latent_dim)
+
+        self.weight_recon_source, self.weight_recon_target = weights
+
+        self.optim = optim.Adam([
+            *self.source_model.parameters(),
+            *self.target_model.parameters(),
+            *self.transition_model.parameters()
+        ])
+
+
+        self.to(self.dev)
+
+    def forward_source(self, xb_source):
+        latent_source = self.source_model.encoder(xb_source)
+        latent_target = self.transition_model(latent_source)
+
+        # TODO: Use class accuracy later
+        # proto_distances_target, _ = self.target_model.proto_layer(latent_target)
+        # prediction_source = self.target_model.predictor(proto_distances_target)
+        
+        transfer_recon_target = self.target_model.decoder(latent_target)
+        transfer_latent_target = self.target_model.encoder(transfer_recon_target)
+
+        # inverse linear transfer (target -> source)
+        transfer_latent_source = (transfer_latent_target - self.transition_model.bias).matmul(torch.inverse(self.transition_model.weight.T))
+        transfer_recon_source = self.source_model.decoder(transfer_latent_source)
+
+        return xb_source, transfer_recon_source
+
+    def forward_target(self, xb_target):
+        latent_target = self.source_model.encoder(xb_target)
+        latent_source = (latent_target - self.transition_model.bias).matmul(torch.inverse(self.transition_model.weight.T))
+
+        # TODO: Use class accuracy later
+        # proto_distances_target, _ = self.target_model.proto_layer(latent_target)
+        # prediction_source = self.target_model.predictor(proto_distances_target)
+        
+        transfer_recon_source = self.source_model.decoder(latent_source)
+        transfer_latent_source = self.source_model.encoder(transfer_recon_source)
+
+        # inverse linear transfer (target -> source)
+        transfer_latent_target = self.transition_model(transfer_latent_source)
+        transfer_recon_target = self.source_model.decoder(transfer_latent_target)
+
+        return xb_target, transfer_recon_target
+
+    def fit_combined_loss(self, source_train_dl, target_train_dl):
+        """
+        Trains using a combined loss for simultaneous optimization
+
+        """
+
+        while self.epoch < self.epochs:
+            self.train()    
+
+            # Loop over target_train and source_train datasets
+            for (xb_target, _), (xb_source, _) in zip(target_train_dl, source_train_dl):
+                xb_target = xb_target.to(self.dev)
+                xb_source = xb_source.to(self.dev)
+
+                # Forward pass for all components
+                _, recon_source = self.forward_source(xb_source)
+                _, recon_target = self.forward_target(xb_target)
+
+                # 1. Loss on transfered source
+                loss_recon_source = self.loss_recon(xb_source, recon_source)
+                print(f'transfer source {self.epoch}: {loss_recon_source}')
+
+                # 2. Loss on transfered target
+                loss_recon_target = self.loss_recon(xb_target, recon_target)
+                print(f'transfer target {self.epoch}: {loss_recon_target}')
+
+                # calculate combined loss
+                self.loss_combined = self.weight_recon_source * loss_recon_source +\
+                                     self.weight_recon_target * loss_recon_target
+                self.loss_combined.backward()
+
+                self.optim.step()
+                self.optim.zero_grad()
+
+            self.epoch += 1
+
+    def loss_pred(self, prediction, label):
+        pred_loss = nn.CrossEntropyLoss()(prediction, label).mean()
+        
+        prediction_class = prediction.argmax(1)
+        num_correct = (prediction_class == label).sum().item()
+        class_accuracy = num_correct / len(prediction)
+
+        return pred_loss, class_accuracy
+
+    def loss_recon(self, input_, recon_image):
+        return F.mse_loss(input_.view(input_.shape[0], -1), recon_image).mean()
+
+    def evaluate(self, target_test_dl):
+        """ Evaluates the classification loss for the target test dataset
+
+        """
+        self.eval()
+        batch_results = None
+        with torch.no_grad():
+            for xb, yb in target_test_dl:
+                xb = xb.to(self.dev)
+                yb = yb.to(self.dev)
+
+                input_, recon_image, prediction, _, _ = self.target_model(xb)
+
+                single_row = len(xb), *self.loss_pred(prediction, yb)
+                if batch_results is None:
+                    batch_results = single_row
+                else:
+                    batch_results = np.vstack((batch_results, single_row))
+
+        size_of_batches, class_loss, class_accuracy = batch_results.T
+        
+        batch_size = np.sum(size_of_batches)
+
+        class_loss = np.sum(np.multiply(class_loss, size_of_batches)) / batch_size
+        class_accuracy = np.sum(np.multiply(class_accuracy, size_of_batches)) / batch_size
+
+        return class_loss, class_accuracy
+
+    def save_model(self, path_name):
+        """ Saves the model, optimizers, loss, and epoch
+
+        More info:
+            https://pytorch.org/tutorials/beginner/saving_loading_models.html
+        """
+        print(f'Saving model to {path_name}')
+        torch.save({
+            'epoch': self.epoch,
+            'model_state_dict': self.state_dict(),
+            'loss_weights': (self.weight_recon_source, self.weight_recon_target)
+            }, path_name)
+
+    @staticmethod
+    def load_model(path_name, model_1, model_2):
+        """
+        Note:
+            If used for inference, make sure to set model.eval()
+        """
+        print(f'Loading model from {path_name}')
+        if torch.cuda.is_available():
+            checkpoint = torch.load(path_name)
+        else:
+            checkpoint = torch.load(path_name, map_location=torch.device('cpu'))
+
+        # configure loss weights if exist
+        if checkpoint['loss_weights']:
+            loaded_model = CycleModel(model_1, model_2, weights=checkpoint['loss_weights'])
+        else:
+            loaded_model = CycleModel(model_1, model_2)
+
+        loaded_model.load_state_dict(checkpoint['model_state_dict'])
+        loaded_model.epoch = checkpoint['epoch']
+
+        return loaded_model
+
+    def visualize_prototypes(self, path_name=None):
+        """ Visualizes the prototypes of both source and target models """
+
+        source_proto = self.source_model.decoder(self.source_model.proto_layer.prototypes)
+        target_proto = self.target_model.decoder(self.target_model.proto_layer.prototypes)
+
+        plot_rows_of_images([source_proto, target_proto], path_name)
+    
+    def visualize_samples(self, source_dl, target_dl, path_name=None):
+        """ Visualizes the samples of both source and target models """
+        xb_source, _ = next(iter(source_dl))
+        xb_target, _ = next(iter(target_dl))
+        xb_source = xb_source.to(self.dev)[:10]
+        xb_target = xb_target.to(self.dev)[:10]
+
+        # latent side
+        latent_source = self.source_model.encoder(xb_source)
+        latent_target = self.target_model.encoder(xb_target)
+
+        # reconstructions
+        recon_source = self.source_model.decoder(latent_source)
+        recon_target = self.target_model.decoder(latent_target)
+
+        _, recon_transfer_source = self.forward_source(xb_source)
+        _, recon_transfer_target = self.forward_target(xb_target)
+
+        plot_rows_of_images([xb_source, xb_target, recon_source, recon_target, recon_transfer_source, recon_transfer_target], path_name)
---
+++ src/scripts/train_cycle.py
@@ -0,0 +1,65 @@
+""" Basic script for training and testing two ProtoModels configured in a CycleModel """
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from data_parsing import load_mnist, load_svhn
+from models.proto_model import ProtoModel
+from models.cycle_model import CycleModel
+
+proto_model_config_1 = {
+    "input_dim": 784,
+    "hidden_layers": [128, 128],
+    "hidden_activations": [nn.ReLU(), None],
+    "latent_dim": 128,
+    "recon_activation": nn.Sigmoid(),
+    "num_prototypes": 10,
+    "num_classes": 10,
+    "use_convolution": False
+}
+
+proto_model_config_2 = {
+    "input_dim": 784,
+    "hidden_layers": [128, 128],
+    "hidden_activations": [nn.ReLU(), None],
+    "latent_dim": 128,
+    "recon_activation": nn.Sigmoid(),
+    "num_prototypes": 10,
+    "num_classes": 10,
+    "use_convolution": True
+}
+
+# Hyperparameters
+NUM_EPOCHS = 10
+BATCH_SIZE = 64
+LEARNING_RATE = 0.001
+
+def train(model_name, epochs=NUM_EPOCHS, train_new=True, save_model=True, weights=(1, 1)):
+
+    # load MNIST data
+    mnist_train_dl, mnist_test_dl = load_mnist.load_mnist_dataloader(BATCH_SIZE)
+    svhn_train_dl, svhn_test_dl = load_svhn.load_svhn_dataloader(BATCH_SIZE, greyscale=True)
+
+    # create ProtoModel
+    model_1 = ProtoModel(proto_model_config_1, LEARNING_RATE)
+    model_2 = ProtoModel(proto_model_config_2, LEARNING_RATE)
+
+    cm = CycleModel(model_1, model_2, epochs=epochs, weights=weights)
+
+    if train_new:
+        cm.fit_combined_loss(mnist_train_dl, svhn_train_dl)
+    else:
+        cm = CycleModel.load_model(f"{model_name}.pth", model_1, model_2)
+
+    res = cm.evaluate(svhn_test_dl)
+    print(res)
+
+    cm.visualize_prototypes(f"{model_name}_proto.jpg")
+    cm.visualize_samples(mnist_train_dl, svhn_train_dl, f"{model_name}_sample.jpg")
+    print(f"weights: {weights}")
+
+    if save_model:
+        cm.save_model(f"{model_name}.pth")
+
+
+if __name__ == "__main__": 
+    train("cm_pure_unsupervised")
