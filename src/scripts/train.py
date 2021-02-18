688d7cdb0e6249b5d7a659077bac13631052b33e
Alex Quach <alexhq1000@gmail.com>
2021-02-17 21:50:13 -0500

    created wrapper for training protomodel

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -16,7 +16,7 @@ proto_model_config = {
     "hidden_activations": [nn.ReLU(), None],
     "recon_activation": nn.Sigmoid(),
 
-    "use_convolution": True,
+    "use_convolution": False,
     "conv_input_channels": 1,
     "conv_hidden_layers": [128, 128],
     "hidden_activations": [nn.ReLU(), None],
@@ -27,31 +27,39 @@ NUM_EPOCHS = 20
 BATCH_SIZE = 64
 LEARNING_RATE = 0.001
 
-# load test data
-train_dl, test_dl = load_svhn.load_svhn_dataloader(BATCH_SIZE, greyscale=True)
+def train(model_name, config=proto_model_config, epochs=NUM_EPOCHS, override_conv=False,\
+    train_new=True, save_model=True):
 
-# create ProtoModel
-model = ProtoModel(proto_model_config, LEARNING_RATE)
-model.to(dev)
+    if override_conv:
+        proto_model_config['use_convolution'] = True
 
-train_new = True
-if train_new:
-    model.fit(NUM_EPOCHS, train_dl, visualize_sample_name="svhn_conv")
-else:
-    model = ProtoModel.load_model("svhn_conv.pth", proto_model_config, LEARNING_RATE)
+    # load test data
+    # train_dl, test_dl = load_svhn.load_svhn_dataloader(BATCH_SIZE, greyscale=True)
+    train_dl, test_dl = load_mnist.load_mnist_dataloader(BATCH_SIZE)
 
-# generate test loss metrics
-train_losses = model.evaluate(train_dl)
-print(train_losses)
-test_losses = model.evaluate(test_dl)
-print(test_losses)
+    # create ProtoModel
+    model = ProtoModel(proto_model_config, LEARNING_RATE)
+    model.to(dev)
 
-model.visualize_sample(test_dl, path_name="svhn_conv_sample.jpg")
-model.visualize_prototypes(path_name="svhn_conv_proto.jpg")
+    if train_new:
+        model.fit(NUM_EPOCHS, train_dl, visualize_sample_name=None)
+    else:
+        model = ProtoModel.load_model("mnist_linear_1.pth", proto_model_config, LEARNING_RATE)
 
-# Testing the save/load functionality
-test_save = True
-if test_save:
-    model.save_model("svhn_conv.pth")
-    new_model = ProtoModel.load_model("svhn_conv.pth", proto_model_config, LEARNING_RATE)
-    #new_model.fit(10, train_dl)
+    # generate test loss metrics
+    train_losses = model.evaluate(train_dl)
+    print(train_losses)
+    test_losses = model.evaluate(test_dl)
+    print(test_losses)
+
+    # model.visualize_sample(test_dl, path_name="svhn_conv_sample.jpg")
+    # model.visualize_prototypes(path_name="svhn_conv_proto.jpg")
+
+    # Testing the save/load functionality
+    if save_model:
+        model.save_model(f"{model_name}.pth")
+        new_model = ProtoModel.load_model(f"{model_name}.pth", proto_model_config, LEARNING_RATE)
+        #new_model.fit(10, train_dl)
+
+if __name__ == "__main__": 
+    train("test", save_model=False)
